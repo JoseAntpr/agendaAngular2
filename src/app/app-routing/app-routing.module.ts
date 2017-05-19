ed50a042b02774c887bc89b3d2093560cacdd5f9
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-05-20 00:27:48 +0200

    Añadido comentarios de app-routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,11 @@ import {Routes, RouterModule} from '@angular/router'
 import {ContactosComponent} from '../contactos/contactos.component'
 import {NuevoContactoComponent} from '../nuevo-contacto/nuevo-contacto.component'
 
+//Definimos las rutas que va a soportar nuestra app. Las rutas tienens como propuedas las siguientes:
+// - path -> ruta a navegar
+// - component -> Componente encargado de responder a la ruta cuando se navega
+// Si queremos controlar rutas no definidas tenemos que indicar como 'path' . doble asterisco
+//Además de establecer el 'pathMatch' como 'full'.
 const routes: Routes = [
   {
     path: "contactos",
